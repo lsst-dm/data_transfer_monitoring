e5202a04e4426b35294540da8c1085c82bf8061c
Zach Hornung <zachhornung@comcast.net>
2025-07-14 14:23:46 -0700

    end readout message from_json should be passed string, not dict

--- listeners/end_readout.py
+++ listeners/end_readout.py
@@ -196,7 +196,7 @@ class EndReadoutListener(BaseKafkaListener):
         log.debug(f"end readout message json: {message}")
         if deserializer:
             message = await deserializer.deserialize(data=message)
-            message = message["message"]
+            message = json.dumps(message["message"])
         msg = EndReadoutModel.from_json(message)
         # if self.should_skip(msg):
         #     return
